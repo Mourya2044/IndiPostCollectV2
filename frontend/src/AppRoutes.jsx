3155e8f91c116d8fe4808c883ede79caef2fc19f
Pavel <pavel153das@gmail.com>
2025-06-19 12:01:13 +0530

    Fixed browsing

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/HomePage'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
 
 
 const AppRoutes = () => {
   return (
     <Routes> 
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signUp" element={<SignUp/>}/>
     </Routes>
   )
 }
