5163d6a45109f563af311ba5c72859eab8097e9a
Mourya <108566691+Mourya2044@users.noreply.github.com>
2025-06-19 11:33:36 +0530

    Add IndiPostCollect SVG and set up routing in Routes component

---
+++ frontend/src/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+
+const AppRoutes = () => {
+  return (
+    <div>Routes</div>
+  )
+}
+
+export default Routes
