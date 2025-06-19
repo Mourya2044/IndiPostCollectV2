031b6c3dfe79674a9b5679ed7e85ef06e6c1f4c5
Mourya <108566691+Mourya2044@users.noreply.github.com>
2025-06-19 12:52:27 +0530

    Refactor index.js to move PORT declaration and add CORS middleware configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import authRouter from './routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
 
 app.use("/api/auth", authRouter)
 
