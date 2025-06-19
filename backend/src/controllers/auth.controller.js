7f091137f991e228f663d9190bb3553afb4c2116
Pavel <pavel153das@gmail.com>
2025-06-19 14:50:45 +0530

    SignUp controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,14 +8,14 @@ const generateToken = (id) => {
 
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
-
+  //verify
   if (!email || !password) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
   try {
     const user = await User.findOne({ email });
-
+    //verify if user present or correct password
     if (!user || !(await user.comparePasswords(password))) {
       return res.status(401).json({ message: "Invalid Credentials" });
     }
@@ -26,18 +26,40 @@ export const loginUser = async (req, res) => {
       token: generateToken(user._id),
     });
   } catch (err) {
-    return res
-      .status(500)
-      .json({ message: "Error logging in", error: err.message });
+    return res.status(500).json({ message: "Error logging in", error: err.message });
   }
 };
 
-export const signupUser = async (req, res) => {
+export const signUpUser = async (req, res) => {
+  const { fullName, email, password, address, district, state, city, pin} = req.body;
+  //verify all fields
+  if(!fullName || !email || !password || !address || !district || !state || !city || !pin){
+    return res.status(400).json({message: "All fields are required"});
+  }
   try {
-    // Logic for user signup
-    res.status(201).json({ message: "Signup successful" });
-  } catch (error) {
-    res.status(500).json({ error: "Signup failed" });
+    const existingUser = await User.findOne({email});//If already exists
+    if(existingUser){
+      return res.status(400).json({message: "Email already in use"});
+    }
+    //Create user
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      address,
+      district,
+      state,
+      city,
+      pin
+    });
+
+    res.status(200).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Error signing in", error: err.message });
   }
 };
 
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
 import {
   loginUser,
-  signupUser,
+  signUpUser,
   logoutUser,
   getUserInfo,
 } from "../controllers/auth.controller.js";
@@ -10,7 +10,7 @@ import {
 const authRouter = express.Router();
 
 authRouter.post("/login", loginUser);
-authRouter.post("/signup", signupUser);
+authRouter.post("/signup", signUpUser);
 authRouter.get("/get", protect, getUserInfo);
 authRouter.post("/logout", logoutUser);
 
