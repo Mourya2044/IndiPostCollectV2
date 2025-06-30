7a7306e0a688537953e52ab916c4f5feda638073
jayaditya30 <177267310+jayaditya77@users.noreply.github.com>
2025-06-30 22:07:44 +0530

    colorchange

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -73,7 +73,7 @@ const SignUp = () => {
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-        <h3 className="text-xl font-semibold text-black">Create Account</h3>
+        <h3 className="text-xl font-semibold text-black">Create  Account</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           {step === 1 ? "Enter your details to create an account" : "Enter your address"}
         </p>
