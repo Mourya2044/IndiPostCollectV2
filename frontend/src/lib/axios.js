c3b4fc8cd9e0362ecd88ee446ceb49446cea5083
Mourya <108566691+Mourya2044@users.noreply.github.com>
2026-03-18 16:07:55 +0530

    Update production API URL in axios instance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://indipostcollectapi.azurewebsites.net/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://indipostcollect.onrender.com/api",
     withCredentials: true
 });
